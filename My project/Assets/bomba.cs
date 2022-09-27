bb4f1394ffaa4c1845d35571b76a8e10179e9a34
Hecthor1975 <leivahector009@gmail.com>
2022-09-27 00:20:45 -0300

    a

--- My project/Assets/bomba.cs
+++ My project/Assets/bomba.cs
@@ -2,32 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bomba : MonoBehaviour
+public class Bomba : MonoBehaviour
 {
-    [SerializeField] private float radio;
-    [SerializeField] private float fuerzadeexplocion;
-    private IEnumerable<Collider2D> Enemigo;
+    [SerializeField] private float velocidad;
 
-    public void Explosion()
+    [SerializeField] private float daño;
+
+    public GameObject hitEffect;
+
+    private void Update()
     {
-        Collider2D[] Enemigo = Physics2D.OverlapCircleAll(transform.position,radio);
+        transform.Translate(Time.deltaTime * velocidad * Vector2.up);
+    }
 
-        foreach (Collider2D colisionador in Enemigo)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemigo"))
         {
-            Rigidbody2D rb2D = colisionador.GetComponent<Rigidbody2D>();
-            if (rb2D != null)
-            {
-                Vector3 direccion = colisionador.transform.position - transform.position;
-                float distancia = 1 + direccion.magnitude;
-                float fuerzaFinal = fuerzadeexplocion / distancia;
-                rb2D.AddForce(direccion * fuerzaFinal);
-            }
+            collision.GetComponent<Enemigo>().TomarDaño(daño);
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.8f);
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
+        else
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.8f);
+            Destroy(gameObject);
+        }
     }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, radio);
-    }
+
 }
